7c6e921103c9f41c35fa4fefe5bedeee52dc6d7c
AlekseyReviakin <it.blackdog@gmail.com>
2014-10-24 18:47:42 +0400

    Test schema introduced

--- src/test/scala/com/cool/url/BaseSpec.scala
+++ src/test/scala/com/cool/url/BaseSpec.scala
@@ -1,6 +1,6 @@
 package com.cool.url
 
-import com.cool.url.config.{ConfigProviderComponent, ProductionConfigProvider}
+import com.cool.url.config.{DbConfig, ConfigProviderComponent, ProductionConfigProvider}
 import com.cool.url.model.{ServiceComponent, SchemaComponent}
 import com.cool.url.service.{QueryExtensions, SlickDbConnectorComponent, DbConnectorComponent, SaltedTokenComponent}
 import org.scalatest.WordSpec
@@ -15,7 +15,15 @@ trait BaseSpec extends WordSpec {
     with QueryExtensions
     with ServiceComponent
     with SchemaComponent {
-    val config = new ProductionConfig
+    val config = new ProductionConfig {
+      override val db = new DbConfig {
+        val host: String = "localhost:5432"
+        val base: String = "url_t"
+        val user: String = "uurl"
+        val password: String = "uurl"
+        val schema = "test"
+      }
+    }
     val connector = new SlickDbConnector
     val tokenService = new SaltedTokenService
   }
