48dd6479bce13bf57b04ab5f077d3701ab022005
AlekseyReviakin <it.blackdog@gmail.com>
2014-10-29 18:45:43 +0300

    Logging workflow

--- src/main/scala/com/cool/url/dao/DaoComponent.scala
+++ src/main/scala/com/cool/url/dao/DaoComponent.scala
@@ -5,6 +5,7 @@ import java.sql.Timestamp
 import com.cool.url.config.ConfigProviderComponent
 import com.cool.url.model.ServiceComponent
 import com.cool.url.service._
+import com.typesafe.scalalogging.StrictLogging
 
 
 trait DaoComponent {
@@ -33,7 +34,7 @@ trait DaoComponent {
 }
 
 
-trait SlickDaoComponent extends DaoComponent {
+trait SlickDaoComponent extends DaoComponent with StrictLogging {
   self: ConfigProviderComponent
     with DbConnectorComponent
     with SlickDbConnectorComponent
@@ -45,62 +46,104 @@ trait SlickDaoComponent extends DaoComponent {
 
   class SlickDaoService extends DaoService {
     def authorizeAndGetToken(target: AuthObject, userId: Long) = if (externalAuthService.isAuthorized(target)) {
+      logger.debug(s"User with userId: $userId requested for a token")
       connector.db.withSession({
         implicit session =>
-          users.getToken(userId)
+          val token = users.getToken(userId)
+          logger.debug(s"User with userId: $userId registered at token: $token")
+          token
       })
     } else {
+      logger.warn(s"User with userId: $userId requested for a token with a fraud secret: $target")
       throw new ExternalAuthFailure(target, ValidationException.INTERNAL_ERROR)
     }
 
 
-    def createLink(token: UserToken, url: String, code: Option[LinkCode], folderId: Option[FolderId]): Link = connector.db.withSession({
-      implicit session => {
-        links.create(token, url, code, folderId)
-      }
-    })
+    def createLink(token: UserToken, url: String, code: Option[LinkCode], folderId: Option[FolderId]) = {
+      logger.debug(s"User with token: $token requested to create link(url: $url, code: $code, folderId: $folderId)")
+      connector.db.withSession({
+        implicit session => {
+          val link = links.create(token, url, code, folderId)
+          logger.debug(s"User with token: $token registered new link $link")
+          link
+        }
+      })
+    }
 
-    def getLink(code: LinkCode, referer: String, remoteIp: String): Link = connector.db.withSession({
-      implicit session => {
-        clicks.addClickForCode(code, new Timestamp(System.currentTimeMillis), referer, remoteIp)
-        links.linkByCode(code)
-      }
-    })
-
-
-    def getLinkAndCountOfClicks(token: UserToken, code: LinkCode): (Link, Int) = connector.db.withSession({
-      implicit session => {
-        links.linkAndClicksCountByCode(token, code)
-      }
-    })
-
-
-    def readLinkByToken(token: UserToken, paging: Option[Paging]): PagingResult[Link] = connector.db.withSession({
-      implicit session => {
-        links.linksByToken(token, paging)
-      }
-    })
+    def getLink(code: LinkCode, referer: String, remoteIp: String) = {
+      logger.debug(s"Click for link with code: $code registration start(referer: $referer, remoteIp: $remoteIp)")
+      connector.db.withSession({
+        implicit session => {
+          val click = clicks.addClickForCode(code, new Timestamp(System.currentTimeMillis), referer, remoteIp)
+          logger.debug(s"Click link with code: $code registered($click)")
+          val link= links.linkByCode(code)
+          logger.debug(s"Click for link with code: $code registration finished(returning link $link)")
+          link
+        }
+      })
+    }
 
 
-    def readClicksByTokenAndCode(token: UserToken, code: LinkCode, paging: Option[Paging]) = connector.db.withSession({
-      implicit session => {
-        links.linkByCodeAndToken(token, code)
-        clicks.linksForCode(code, paging)
-      }
-    })
+    def getLinkAndCountOfClicks(token: UserToken, code: LinkCode): (Link, Int) = {
+      logger.debug(s"User with token: $token requested to obtain link and count of clicks for link code: $code")
+      connector.db.withSession({
+        implicit session => {
+          val (link, clicks) = links.linkAndClicksCountByCode(token, code)
+          logger.debug(s"User with token: $token obtained link and count of clicks: ($link, $clicks)")
+          (link, clicks)
+        }
+      })
+    }
 
-    def readLinksByFolder(token: UserToken, folderId: FolderId, paging: Option[Paging]) = connector.db.withSession({
-      implicit session => {
-        folders.folderByTokenAndId(token, folderId)
-        links.linksByFolder(folderId, paging)
-      }
-    })
 
-    def readFoldersByToken(token: UserToken, paging: Option[Paging]) = connector.db.withSession({
-      implicit session => {
-        folders.foldersByToken(token, paging)
-      }
-    })
+    def readLinkByToken(token: UserToken, paging: Option[Paging]): PagingResult[Link] = {
+      logger.debug(s"User with token: $token requested to obtain links with paging: $paging")
+      connector.db.withSession({
+        implicit session => {
+          val result = links.linksByToken(token, paging)
+          logger.debug(s"User with token: $token obtained links with paging: $paging, links count is ${result.count}")
+          result
+        }
+      })
+    }
+
+
+    def readClicksByTokenAndCode(token: UserToken, code: LinkCode, paging: Option[Paging]) = {
+      logger.debug(s"User with token: $token requested to read clicks for link code: $code with paging $paging")
+      connector.db.withSession({
+        implicit session => {
+          val link = links.linkByCodeAndToken(token, code)
+          logger.debug(s"User with token: $token requested to read clicks for link $link")
+          val result = clicks.linksForCode(code, paging)
+          logger.debug(s"User with token: $token obtained clicks for link $link, clicks count is ${result.count}")
+          result
+        }
+      })
+    }
+
+    def readLinksByFolder(token: UserToken, folderId: FolderId, paging: Option[Paging]) = {
+      logger.debug(s"User with token: $token requested requested to read folder content of folder with id $folderId, with paging $paging")
+      connector.db.withSession({
+        implicit session => {
+          val folder = folders.folderByTokenAndId(token, folderId)
+          logger.debug(s"User with token: $token requested requested to read folder content of folder: $folder")
+          val result = links.linksByFolder(folderId, paging)
+          logger.debug(s"User with token: $token obtained folder content of folder: $folder, links count is ${result.count}")
+          result
+        }
+      })
+    }
+
+    def readFoldersByToken(token: UserToken, paging: Option[Paging]) = {
+      logger.debug(s"User with token: $token requested to read folders, with paging $paging")
+      connector.db.withSession({
+        implicit session => {
+          val result = folders.foldersByToken(token, paging)
+          logger.debug(s"User with token: $token obtained folders, count is ${result.count}")
+          result
+        }
+      })
+    }
   }
 
 }
